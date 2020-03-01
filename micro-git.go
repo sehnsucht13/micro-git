58ac1a5bd01810d1c140a92eb0ac36bd6f65fc20
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-02-29 18:28:56 -0800

    Implemented initialization of bare repository
    - Add helper function to detect if a folder exists
    - Implement micro-git init to create a new repository

--- micro-git.go
+++ micro-git.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
-func initRepo()
+func dirExists(dirPath string) bool {
+	_, err := os.Stat(dirPath)
 
-func main(){
-	fmt.Println("hello world")
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func InitRepo(dirPath string, quiet bool) {
+	currPath, err := filepath.Abs(dirPath)
+	if err != nil {
+		fmt.Println("Error")
+	}
+	repoFolder := filepath.Join(currPath, ".micro-git")
+	if dirExists(repoFolder) == false {
+		// Create all directories
+		os.MkdirAll(filepath.Join(repoFolder, "objects"), 0755)
+		os.Mkdir(filepath.Join(repoFolder, "branches"), 0755)
+		os.Mkdir(filepath.Join(repoFolder, "refs"), 0755)
+		os.Mkdir(filepath.Join(repoFolder, "refs", "heads"), 0755)
+		os.Mkdir(filepath.Join(repoFolder, "refs", "tags"), 0755)
+		// Create files
+		if !quiet {
+			fmt.Println("Repository initialized in", string(currPath), "successfully!")
+		}
+		os.Exit(0)
+	}
+	fmt.Println(".micro-git folder already exists in", currPath, "Repository cannot be initialized!")
+	os.Exit(1)
+}
+
+func main() {
+	InitRepo(".", false)
 }
