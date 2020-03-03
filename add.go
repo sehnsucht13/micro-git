a16106b8cdbeeb9828d404093637b721ffb77c95
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-03-02 22:21:33 -0800

    Implement recursive add for folders and files
    - Implement recurisively adding folders and files using "micro-git add"
      command

--- add.go
+++ add.go
@@ -5,75 +5,105 @@ import (
 	"compress/zlib"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
-	"errors"
 	"strconv"
 )
 
-
-func hashFile(fileName string) string {
+// Create a unique hash for string using SHA1
+func hashString(stringContent string) string {
 	h := sha1.New()
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Cannot access file", file, "Check if the file exists and access permitions are set.")
-		os.Exit(2)
-	}
-	defer file.Close()
-	if _, err := io.Copy(h, file); err != nil {
-		log.Fatal(err)
-	}
+	io.WriteString(h, stringContent)
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func compressFileContents(fileName string) []byte {
-	contents, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Println("Could not open file!")
-	}
+// Comress a string using zlib and return its compressed form
+func compressString(stringContent string) []byte {
 	var compressedBuff bytes.Buffer
 	zWriter := zlib.NewWriter(&compressedBuff)
-	zWriter.Write(contents)
+	zWriter.Write([]byte(stringContent))
 	zWriter.Close()
 	return compressedBuff.Bytes()
 }
 
-func storeFile(filePath string) (IndexEntry, error){
+// Compress and store a new file containing fileContent in the .micro-git/objects folder
+// Returns an IndexEntry containing its hash. 
+// If this function is called from a directory which is not a micro-git repository
+// then return error
+func addObjectFile(fileContent string) (IndexEntry, error){
 	index_entry := IndexEntry{entry_type: blob}
 	repoRoot, err := FindRepoRoot()
-	if err != nil {
-		fmt.Println("Not in a repo")
-		os.Exit(1)
+	if err != nil{
+		return index_entry, errors.New("Not currently visiting a repository!")
 	}
+	sha1String := hashString(fileContent)
+	compString := compressString(fileContent)
 
-	file, err := os.Stat(filePath)
-	if err != nil {
-		//fmt.Println("File located at", filePath, "cannot be added.")
-		return index_entry, errors.New("File cannot be accessed " + file.Name())
-	}
-	sha1String := hashFile(filePath)
-	compFile := compressFileContents(filePath)
 	// Create folder to hold new blob/tree and add compressed file
 	blobFolderPath := filepath.Join(repoRoot, ".micro-git", "objects", sha1String[0:2])
 	os.Mkdir(blobFolderPath, 0755)
-	ioutil.WriteFile(filepath.Join(blobFolderPath, sha1String[2:]), compFile, 0755)
-
-	index_entry.SetPerm("0" + strconv.FormatUint(uint64(file.Mode().Perm()), 8))
+	ioutil.WriteFile(filepath.Join(blobFolderPath, sha1String[2:]), compString, 0755)
 	index_entry.SetHash(sha1String)
-	index_entry.SetName(file.Name())
 	return index_entry, nil
 }
 
-/*
-func AddFiles(filePaths ...string) {
-	repoRoot, err := FindRepoRoot()
+func storeFile(absPath, relPath string) (IndexEntry, error) {
+	file_stat, err := os.Stat(absPath)
 	if err != nil {
-		fmt.Println("Not in a repo")
-		os.Exit(1)
+		return IndexEntry{}, errors.New("File cannot be accessed " + relPath)
 	}
+	file_contents, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		return IndexEntry{}, errors.New("File cannot be accessed " + relPath)
+	}
+
+	index_entry, err := addObjectFile(string(file_contents))
+	index_entry.SetPerm("0" + strconv.FormatUint(uint64(file_stat.Mode().Perm()), 8))
+	index_entry.SetName(relPath)
+	return index_entry, nil
 }
-*/
+
+func addDirectory(dirAbsPath, dirRelPath string) ([]IndexEntry, error){
+	var indexEntries []IndexEntry
+	dirFiles, _ := ioutil.ReadDir(dirAbsPath)
+	for _, file  := range dirFiles{
+		if fileStat, err := os.Stat(filepath.Join(dirAbsPath, file.Name())); err == nil{
+			if fileStat.IsDir(){
+				subDirEntries, _ := addDirectory(filepath.Join(dirAbsPath, file.Name()), filepath.Join(dirRelPath, file.Name()))
+				indexEntries = append(indexEntries, subDirEntries...)
+			}else{
+				entry, _ := storeFile(filepath.Join(dirAbsPath, file.Name()), filepath.Join(dirRelPath, file.Name()))
+				indexEntries = append(indexEntries, entry)
+			}
+		}
+	}
+	return indexEntries, nil
+}
+
+func AddFiles(filePaths []string){
+	var indexEntries []IndexEntry
+	if !IsRepo(){
+		fmt.Println("Not in a repo")
+		return
+	}
+	for _, path := range filePaths{
+		absPath, _ := filepath.Abs(path)
+		if fileStat, err := os.Stat(absPath); err == nil{
+			relPath := FindRelPath(absPath)
+			if fileStat.IsDir(){
+				fmt.Println(absPath, relPath)
+				dirEntries, _ := addDirectory(absPath, relPath)
+				indexEntries = append(indexEntries, dirEntries...)
+			}else{
+				entry, _ := storeFile(absPath, relPath)
+				indexEntries = append(indexEntries, entry)
+			}
+		}
+	}
+
+	fmt.Println("Index Entries", indexEntries)
+}
