b5a99d025d3673e9a1bd7adcbf11614b996576b4
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-03-02 23:56:42 -0800

    Implement decompression of object files
    - Implement decompression of object files using zlib.

--- cat-file.go
+++ cat-file.go
@@ -1,52 +1,67 @@
 package main
 
 import (
-"strings"
-"fmt"
-"errors"
-"path/filepath"
-"io/ioutil"
-"os"
+	"bytes"
+	"compress/zlib"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // TODO: Error handling
-func findFileByHash(hash string) (string, error){
+func findFileByHash(hash string) (string, error) {
 	var hashFilePath string
 	repoRoot, _ := FindRepoRoot()
 	objectFolderPath := filepath.Join(repoRoot, ".micro-git", "objects")
 
 	dirFiles, _ := ioutil.ReadDir(objectFolderPath)
-	for _, file  := range dirFiles{
+	for _, file := range dirFiles {
 		hashDirFile, _ := ioutil.ReadDir(filepath.Join(objectFolderPath, file.Name()))
-		fmt.Println( strings.Join([]string{file.Name(), hashDirFile[0].Name()},""))
-		if len(hashDirFile) == 0{
-			return "",errors.New("micro-git objects folder located at " + objectFolderPath + " is corrupted!")
+		fmt.Println(strings.Join([]string{file.Name(), hashDirFile[0].Name()}, ""))
+		if len(hashDirFile) == 0 {
+			return "", errors.New("micro-git objects folder located at " + objectFolderPath + " is corrupted!")
 		}
-		if strings.HasPrefix(strings.Join([]string{file.Name(), hashDirFile[0].Name()},""), hash){
-			if hashFilePath != ""{
-				return "",errors.New("SHA1 hash provided is ambigous.")
+		if strings.HasPrefix(strings.Join([]string{file.Name(), hashDirFile[0].Name()}, ""), hash) {
+			if hashFilePath != "" {
+				return "", errors.New("SHA1 hash provided is ambigous.")
 			}
 			hashFilePath = filepath.Join(objectFolderPath, file.Name(), hashDirFile[0].Name())
 		}
 	}
-	if hashFilePath == ""{
+	if hashFilePath == "" {
 		return "", errors.New("File corresponding to provided SHA1 hash could not be found!")
 	}
 	fmt.Println("Found file with ", hashFilePath)
 	return hashFilePath, nil
 }
 
-func getObjectSize(hash string) int64{
+func getObjectSize(hash string) int64 {
 	path, err := findFileByHash(hash)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return -1
 	}
 	if file, _ := os.Stat(path); err == nil {
 		return file.Size()
-	}else{
+	} else {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	return -1
+}
+
+func decompressFileContents(path string) string {
+	fileContents, _ := os.Open(path)
+	var out bytes.Buffer
+	fmt.Println(string(fileContents.Name()))
+
+	reader, _ := zlib.NewReader(fileContents)
+	defer reader.Close()
+	io.Copy(&out, reader)
+	fmt.Println("The contents are: ", string(out.Bytes()))
+	return ""
 }
