77f8d0356b84c12e68eaa24a43d01296c57b2415
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-03-15 16:51:24 -0700

    Implement basic functions for hash-objects subcommand
    - Implement basic functions to:
      - Hash objects from stdin
      - Hash objects with path provided through stdin
      - Hash objects provided through command line arguments

---
+++ hash-object.go
@@ -0,0 +1,66 @@
+// Commands related to micro-git hash-object.
+// Support is provided for:
+// - "-w" flag
+// - "--stdin" flag
+// - "--stdin-paths" flag
+// - Regular usage without any flag
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func hashObjectAtPath(path string) (string, error) {
+	readPermission, err := IsReadable(path)
+	if err != nil || !readPermission {
+		return "", errors.New("File does not exist or cannot be read.")
+	}
+	fileContents, _ := ioutil.ReadFile(path)
+	return hashString(string(fileContents)), nil
+}
+
+func hashObjectStdin() {
+	var inputString strings.Builder
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		inputString.WriteString(scanner.Text())
+	}
+	fmt.Println(hashString(inputString.String()))
+}
+
+func hashObjectStdinPath() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		filePath := scanner.Text()
+		fileHash, err := hashObjectAtPath(filePath)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(fileHash)
+	}
+}
+
+func HashObject(filePaths []string, writeFlag, stdin, stdinPath bool) {
+	if stdin {
+		hashObjectStdin()
+		os.Exit(0)
+	} else if stdinPath {
+		os.Exit(1)
+	}
+
+	if len(filePaths) > 1 {
+		for _, path := range filePaths {
+			hash, err := hashObjectAtPath(path)
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println(hash)
+			}
+		}
+	}
+}
